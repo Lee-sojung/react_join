a0d541c17ecb8c2299da9ff5fed5c4a0ef869f51
Lee-sojung <thwjd3698@gmail.com>
2022-01-21 11:55:19 +0900

    자식 컴포넌트에서 dispatch로 reducer데이터 변경 요청

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,8 +1,22 @@
 import { useEffect } from "react";
 import { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import {setMembers} from "../../redux/actions";
 
 function Department(){
   const frame = useRef(null);
+
+  const members = useSelector(state=>state.memberReducer.members);
+  console.log(members);
+  const dispatch = useDispatch();
+
+  const newMember =[
+    {name: 'tom', position: 'CEO'},
+    {name: 'Emma', position: 'Designer'},
+    {name: 'Michael', position: 'Developer'}
+]
+
 
   useEffect(()=>{
     frame.current.classList.add('on');
@@ -11,7 +25,9 @@ function Department(){
     <main ref={frame}>
       <div className="inner">
         <h1><a href="#">Department</a></h1>
-
+        <button onClick={()=>{
+          dispatch(setMembers(newMember))
+        }}> 멤버변경</button>
       </div>
     </main>
   )
