dee698f108ae215b1eaedd321f7b92177d651cf6
Lee-sojung <thwjd3698@gmail.com>
2022-01-24 10:26:00 +0900

    redux-persist로 redux데이터 로컬스토리지에 저장해서 새로고침해도 상태값유지

--- src/components/main/Info.js
+++ src/components/main/Info.js
@@ -1,7 +1,47 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useSelector, useDispatch} from 'react-redux';
+import {setYoutube} from '../../redux/actions';
+
 function Info(){
+  const youtube = useSelector(state=>state);
+  const dispatch = useDispatch();
+  const vidData = youtube.youtubeReducer.youtube;
+
+  //유투브 api호출시 url 옵션값
+  const api_key = "AIzaSyB7VIAECTixPlj0sr-goHwkmNRFIwxZntA";
+  //const playListId = "PLYOPkdUKSFgX5CgKf68RJzJHec0XEdBNd";
+  const playListId = "PLGOVj4gmzJyBMQSKPpBoycEvgXVFPMRZV";
+  const num = 10;
+  const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${api_key}&playlistId=${playListId}&maxResults=${num}`; 
+
+  //youtube데이터를 axios로 호출해서 duspatch를 통해 reducer에 전달하는 함수 정의
+  const fetchYoutube = async ()=>{
+    const response = await axios
+    .get(url)
+    .catch(err=> console.error(err))
+
+    dispatch(setYoutube(response.data.items));
+  }
+
+  //컴포넌트 생성시 fetchYoutube를 호출해 reducer에 햐당 데이터 전달
+  useEffect(()=>{
+    fetchYoutube();
+  },[])
   return (
     <section id="info"className="myScroll">
-      
+      <div className="inner">
+        <h2>RECENT YOUTUBE</h2>
+        <div className="vidBox">
+          {
+            vidData.map((vid,index)=>{
+              return (
+                <img key={index} src={vid.snippet.thumbnails.medium.url} />
+              )
+            })
+          }
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -4,27 +4,19 @@
   3. 각각의 가상DOM요소 클릭시 레이어팝업 동적으로 생성
   4. 해당 레이어팝업 안쪽에 데이터와, 순서관련 state값을 활용해서 세부 컨텐츠 출력
 */
-import axios from 'axios';
-import {useEffect, useState} from 'react';
-import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from 'react/cjs/react.production.min';
+import {useEffect, useRef, useState} from 'react';
+import { useSelector } from 'react-redux';
 
 function Youtube(){
-  let [data, setData] = useState([]);
+  const frame = useRef(null);
   let [isPop, setIsPop] = useState(false);
   let [index, setIndex] = useState(0);
 
-  const api_key = "AIzaSyB7VIAECTixPlj0sr-goHwkmNRFIwxZntA";
-  //const playListId = "PLYOPkdUKSFgX5CgKf68RJzJHec0XEdBNd";
-  const playListId = "PLGOVj4gmzJyBMQSKPpBoycEvgXVFPMRZV";
-  const num = 10;
-  const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${api_key}&playlistId=${playListId}&maxResults=${num}`; 
+  const youtube = useSelector(state=>state);
+  const vidData = youtube.youtubeReducer.youtube;
 
   useEffect(()=>{
-    axios
-      .get(url)
-      .then(json=>{    
-        setData(json.data.items);        
-      })
+    frame.current.classList.add('on');
   },[]);
 
   return (
@@ -34,7 +26,7 @@ function Youtube(){
 
         <section className="frame">
           {            
-            data.map((item, index)=>{
+            vidData.map((item, index)=>{
               let tit = item.snippet.title;
               let tit_len = tit.length;
               let desc = item.snippet.description;
@@ -69,7 +61,7 @@ function Youtube(){
     return (
       <aside className="pop">
         <iframe 
-          src={"https://www.youtube.com/embed/"+data[index].snippet.resourceId.videoId}  width='100%' height='100%' allowFullScreen
+          src={"https://www.youtube.com/embed/"+vidData[index].snippet.resourceId.videoId}  width='100%' height='100%' allowFullScreen
         ></iframe>
         <span onClick={()=>{
           setIsPop(false);
